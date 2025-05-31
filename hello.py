085b7726405a279890287bd048ddbda9c5570814
deviant-rat <sharovdanny01@gmail.com>
2025-05-31 22:45:59 +0300

    Input errors handler added

--- hello.py
+++ hello.py
@@ -1,17 +1,24 @@
 import random
 
 print("hello")
-randon_int = random.randint(1,15)
+random_int = random.randint(1,15)
 count = 0
 while(True):
-    user_guess = int(input('Plese enter a number: '))
+    try:
+       user_guess = int(input('Plese enter a number between 1 and 15: '))
+    except ValueError:
+        print("That's not a valid number! Please type a number ")
+        continue  # Skip the rest of the loop and ask for input again
+    if(user_guess < 1 or user_guess > 15):
+        print("Your number is not in the range!")
+        continue
     count+=1
-    if (user_guess>randon_int):
+    if (user_guess>random_int):
         print("Aim lower")
-    elif(user_guess<randon_int):
+    elif(user_guess<random_int):
         print("Aim higher")
     else:
         print("You won!!!")
         print("Number of guesses: "+str(count))
         break
-print(randon_int)
+#print(randon_int)
